12a75f98ccb385204356924126d98d1cc43e9bff
YangTianCi <tianci@youxuanbao.cn>
2018-07-12 13:17:09 +0800

    添加注释

--- LinkChainUIKit/ViewController.m
+++ LinkChainUIKit/ViewController.m
@@ -35,12 +35,15 @@
 
     
     NSString *contentStr = @"  创建一个这样的界面, 就只需要以上代码即可, 相对以前的代码, 减少了很多不必要的换行, 并且, 将大部分属性设置放置在block中, 如果想要更改对应的属性, 直接搜索属性名称即可, 不会出现搜索出来一大片的弊端, 因为在编写的时候采用的方法是建立于苹果允许的语法规则之上的, 所以不会出现不兼容的情况, 并且, 因为是类似点语法形式的编写,所以,也可以适当的锻炼一下没有swift经验的同学.\n  相对于带来的好处来说,这个类库的学习成本实际上并没有那么高, 仅仅是在原系统的属性之上添加了类库专用的代号, 作用仅仅是防止与系统名称发生冲突.\n 最后,这个项目目前的覆盖面仅仅是UI控件的属性以及函数, 之后会持续更新, 保证覆盖更多的类库, 如果你对本类库有指导意见或者加入意愿, 联系Q.624946274";
-
+    
     CGFloat fontHeight = [contentStr boundingRectWithSize:CGSizeMake(kScreenWidth - 2 * kMargin, MAXFLOAT) options:NSStringDrawingUsesLineFragmentOrigin attributes:@{NSFontAttributeName : [UIFont systemFontOfSize:15]} context:nil].size.height;
     
     self.BGImgView = [UIImageView tc_InitWithBlock:^(UIImageView *imgView) {
        imgView.tc_FrameRect(self.view.bounds).tc_ImageName(@"1").tc_SuperView(self.view).tc_ContentMode(UIViewContentModeScaleAspectFit);
     }];
+    
+    id blcok = self.BGImgView.tc_Tag(3);
+    
     
     self.ExampleImgView = [UIImageView tc_InitWithBlock:^(UIImageView *imgView) {
         imgView.tc_ImageName(@"111").tc_SuperView(self.view).tc_FrameValue(kMargin, kMargin * 3, kScreenWidth - 2 * kMargin, 150).tc_ContentMode(UIViewContentModeScaleAspectFit);
